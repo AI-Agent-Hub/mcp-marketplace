5513743608add9e4331e4956f82dcd5433031c89
AI-Agent-Hub <aihubagent@163.com>
2025-06-19 06:54:29 +0800

    Create constants.py

---
+++ app/mcp_tool_use/src/constants.py
@@ -0,0 +1,60 @@
+from pathlib import Path
+
+ROOT_DIR = Path(__file__).parent.parent
+DATA_DIR = ROOT_DIR / "data"
+SRC_DIR = ROOT_DIR / "src"
+
+## local managed mcp config
+MCP_CONFIG_LOCAL_JSON_PATH = DATA_DIR/ "mcp/config/mcp_config.json"
+MCP_CONFIG_LOCAL_CATEGORY_JSON_PATH = DATA_DIR/ "mcp/config/category"
+## Marketplace Web-Based MCP Config
+MCP_CONFIG_MARKETPLACE_JSON_PATH = DATA_DIR/ "mcp/mcp_marketplace/mcp_config.json"
+MCP_CONFIG_LOCAL_CACHE_FOLDER = DATA_DIR/ "mcp/mcp_marketplace/files"
+MCP_TOOLS_FOLDER_PATH = DATA_DIR/ "mcp/tools/schema"
+
+## MCP Marketplace Config
+MCP_MARKETPLACE_MCP_CONFIG_FOLDER = DATA_DIR/ "mcp/mcp_marketplace"
+MCP_MARKETPLACE_MCP_CONFIG_NAME = "mcp_config.json"
+MCP_MARKETPLACE_MCP_SERVER_META_PATH = DATA_DIR/ "mcp/mcp_marketplace/preinstall_mcp_servers_meta.json"
+
+OUTPUT_FORMAT_SWITCH = "html"
+KEY_RESPONSE_TYPE = "response_type"
+RESPONSE_TYPE_TEXT = "text"
+RESPONSE_TYPE_HTML = "html"
+OUTPUT_FORMAT_HTML = "html"
+OUTPUT_FORMAT_MARKDOWN = "markdown"
+OUTPUT_FORMAT_TEXT = "text"
+MOCK_RESPONSE_SWITCH = False
+MCP_INIT_AUTO_ENABLE = False
+
+BRANCH_NORMAL = 0
+BRANCH_AGENT_ACTION = 1
+
+KEY_CONTEXT = "context"
+KEY_THINK ="think"
+KEY_TOOL_ID = "tool_id"
+KEY_TOOL_NAME = "tool_name"
+KEY_TOOL_ARGUMENTS = "tool_arguments"
+KEY_TOOLNAME_TO_SERVER_DICT = "toolname_to_server_dict"
+
+SECTION_THINK = "think"
+SECTION_ANSWER = "answer"
+SECTION_TOOL = "tool"
+SECTION_SYSTEM_MSG = "system_msg"
+SECTION_CONTEXT = "context"
+
+KEY_MODEL_SELECTION = "model_selection"
+MODEL_SELECTION_CLAUDE_37 = "Claude"
+MODEL_SELECTION_QWEN_MAX = "Qwen"
+MODEL_SELECTION_DEEPSEEK = "DeepSeek"
+MODEL_SELECTION_DEFAULT = "Claude"
+
+KEY_MCP_JSON_SERVERS = "mcpServers"
+MCP_TYPE_SSE = "SSE"
+MCP_TYPE_STDIO = "STDIO"
+MCP_CALL_TOOL_TIMEOUT = 15.0
+
+KEY_SUCCESS = "success"
+KEY_CONTENT = "content" 
+KEY_MESSAGE = "message"
+
